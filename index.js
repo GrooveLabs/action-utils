e5e1e83cec7d335b71ef65ff18222ef6f189984a
Vijay Pemmaraju <vijayvmp@gmail.com>
2017-10-16 10:09:21 -0700

    added index file

---
+++ index.js
@@ -0,0 +1,35 @@
+function createActionsHelper(actionKey, actions) {
+  if (typeof actions === 'string') {
+    if (actionKey) {
+      return `${actionKey}/${actions}`;
+    }
+
+    return actions;
+  }
+  return Object.entries(actions).reduce((previous, entry) => {
+    const [key, value] = entry;
+    if (key === value) return { ...previous, [key]: createActionsHelper(actionKey, value) };
+    let newKey = key;
+    if (actionKey) {
+      newKey = `${actionKey}/${key}`;
+    }
+    return { ...previous, [key]: createActionsHelper(newKey, value) };
+  }, {});
+}
+
+export function createActions(actions) {
+  return createActionsHelper(null, actions);
+}
+
+export function keyMirror(keys) {
+  return keys.reduce((previous, key) => ({ ...previous, [key]: key }), {});
+}
+
+export function standardActions() {
+  return keyMirror([
+    'BEGIN',
+    'PROGRESS',
+    'SUCCESS',
+    'FAILURE',
+  ]);
+}
