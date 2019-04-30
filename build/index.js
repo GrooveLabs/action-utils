1cc37ce835764ae9dbf12fa11fbb2b62b0bdc933
Vijay Pemmaraju <vijayvmp@gmail.com>
2019-04-30 11:40:42 -0700

    add back build for smoother transition

---
+++ build/index.js
@@ -0,0 +1,32 @@
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function createActionsHelper(actionKey, actions) {
+  if (typeof actions === 'string') {
+    if (actionKey) {
+      return `${actionKey}/${actions}`;
+    }
+
+    return actions;
+  }
+  return Object.entries(actions).reduce((previous, entry) => {
+    const [key, value] = entry;
+    if (key === value) return _extends({}, previous, { [key]: createActionsHelper(actionKey, value) });
+    let newKey = key;
+    if (actionKey) {
+      newKey = `${actionKey}/${key}`;
+    }
+    return _extends({}, previous, { [key]: createActionsHelper(newKey, value) });
+  }, {});
+}
+
+export function createActions(actions) {
+  return createActionsHelper(null, actions);
+}
+
+export function keyMirror(keys) {
+  return keys.reduce((previous, key) => _extends({}, previous, { [key]: key }), {});
+}
+
+export function standardActions() {
+  return keyMirror(['BEGIN', 'PROGRESS', 'SUCCESS', 'FAILURE', 'CANCEL']);
+}
