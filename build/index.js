6a4dcaaa55425a5f9bae62d82000c7434afdeff6
Zac Brown <zac@grooveapp.com>
2020-03-05 17:04:41 -0800

    use amd not es6

--- build/index.js
+++ build/index.js
@@ -9,41 +9,48 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-function createActionsHelper(actionKey, actions) {
-    if (typeof actions === 'string') {
-        if (actionKey) {
-            return actionKey + "/" + actions;
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function createActionsHelper(actionKey, actions) {
+        if (typeof actions === 'string') {
+            if (actionKey) {
+                return actionKey + "/" + actions;
+            }
+            return actions;
         }
-        return actions;
+        return Object.entries(actions).reduce(function (previous, _a) {
+            var _b, _c;
+            var key = _a[0], value = _a[1];
+            if (key === value || value === '')
+                return __assign(__assign({}, previous), (_b = {}, _b[key] = createActionsHelper(actionKey, key), _b));
+            var newKey = key;
+            if (actionKey) {
+                newKey = actionKey + "/" + key;
+            }
+            return __assign(__assign({}, previous), (_c = {}, _c[key] = createActionsHelper(newKey, value), _c));
+        }, {});
     }
-    return Object.entries(actions).reduce(function (previous, _a) {
-        var _b, _c;
-        var key = _a[0], value = _a[1];
-        if (key === value || value === '')
-            return __assign(__assign({}, previous), (_b = {}, _b[key] = createActionsHelper(actionKey, key), _b));
-        var newKey = key;
-        if (actionKey) {
-            newKey = actionKey + "/" + key;
-        }
-        return __assign(__assign({}, previous), (_c = {}, _c[key] = createActionsHelper(newKey, value), _c));
-    }, {});
-}
-export function createActions(actions) {
-    return createActionsHelper(null, actions);
-}
-export function keyMirror(keys) {
-    return keys.reduce(function (previous, key) {
-        var _a;
-        return (__assign(__assign({}, previous), (_a = {}, _a[key] = key, _a)));
-    }, {});
-}
-export function standardActions() {
-    return keyMirror([
-        'BEGIN',
-        'PROGRESS',
-        'SUCCESS',
-        'FAILURE',
-        'CANCEL',
-    ]);
-}
+    function createActions(actions) {
+        return createActionsHelper(null, actions);
+    }
+    exports.createActions = createActions;
+    function keyMirror(keys) {
+        return keys.reduce(function (previous, key) {
+            var _a;
+            return (__assign(__assign({}, previous), (_a = {}, _a[key] = key, _a)));
+        }, {});
+    }
+    exports.keyMirror = keyMirror;
+    function standardActions() {
+        return keyMirror([
+            'BEGIN',
+            'PROGRESS',
+            'SUCCESS',
+            'FAILURE',
+            'CANCEL',
+        ]);
+    }
+    exports.standardActions = standardActions;
+});
 //# sourceMappingURL=index.js.map
