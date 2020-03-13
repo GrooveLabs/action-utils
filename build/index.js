0094a08237b3cc064c98b8879bf3fb8d96428e9f
Zac Brown <zac@grooveapp.com>
2020-03-13 16:09:07 -0700

    fix(tsc): use umd for Jest support

--- build/index.js
+++ build/index.js
@@ -9,7 +9,15 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-define(["require", "exports"], function (require, exports) {
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     function createActionsHelper(actionKey, actions) {
